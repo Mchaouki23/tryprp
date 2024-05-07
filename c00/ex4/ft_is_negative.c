bb2dc5c2e92455031fcb6e33a08f37e3045e14f5
malikat <chawqimalika3@gmail.com>
2024-05-07 19:40:47 +0100

    np

--- c00/ex4/ft_is_negative.c
+++ c00/ex4/ft_is_negative.c
@@ -1,18 +1,20 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void ft_is_negative(int n)
 {
-
-	write(1, &n, 1);
-		
-	
+	if( n < 0)
+	{
+		write(1, "N", 1);
+	}
+	else
+		write(1, "P", 1);
 }
 
 int main()
 {
-	int n = 7;
-	write(1, '5', 1);
-	ft_is_negative(n);
+	
+	ft_is_negative(-8);
 }
 
 
